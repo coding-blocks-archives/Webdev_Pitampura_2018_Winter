3663aad626d164d09edbbc2cce06e25729f924ab
Arnav Gupta <arnav@codingblocks.com>
2019-01-13 17:05:14 +0530

    dynamic title
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture13/todo-hbs/server.js
+++ Lecture13/todo-hbs/server.js
@@ -14,6 +14,7 @@ app.set('view engine', 'hbs')
 app.get('/tasks', (req, res) => {
   // res.send(tasks.getTasks())
   res.render('tasks', {
+    title: 'Task List',
     tasks: tasks.getTasks()
   })
 })
